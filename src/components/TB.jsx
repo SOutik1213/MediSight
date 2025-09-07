83e161840186b0eb9b91787367375bdb6884a11a
Soutik <113308816+SOutik1213@users.noreply.github.com>
2025-09-07 10:38:48 +0530

    Update TB.jsx

--- src/components/TB.jsx
+++ src/components/TB.jsx
@@ -7,7 +7,7 @@ const TB = ({ onBack }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const API_URL = 'http://localhost:5000/api/predict/tb';
+  const API_URL = `${import.meta.env.VITE_API_URL}/api/predict/tb`;
 
 
   // Drag-and-drop support
